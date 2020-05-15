90a7c10d93eeb09d629d125754101281dba0d089
Rachel Schieferstein <violenza@gmail.com>
2020-05-15 11:05:49 -0700

    updating api calls to async/await to clean up code

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
   res.send({'helddslo': 'jodhnny'});
 });
 
-app.post('/api/df_text_query', (req, res) => {
+app.post('/api/df_text_query', async (req, res) => {
 
   const request = {
     session: sessionPath,
@@ -21,27 +21,14 @@ app.post('/api/df_text_query', (req, res) => {
       },
     },
   };
-  sessionClient
+  let responses = await sessionClient
     .detectIntent(request)
-    .then(responses => {
-        console.log('Detected intent');
-        const result = responses[0].queryResult;
-        console.log(`  Query: ${result.queryText}`);
-        console.log(`  Response: ${result.fulfillmentText}`);
-        if (result.intent) {
-            console.log(`  Intent: ${result.intent.displayName}`);
-        } else {
-            console.log(`  No intent matched.`);
-        }
-    })
-    .catch(err => {
-        console.error('ERROR:', err);
-    });
-  res.send({'do': 'text query'})
-});
 
-app.post('api/df_event_query', (req, res) => {
-  res.send({'do': 'event query'})
-})
+  res.send(responses[0].queryResult)
+  });
+
+  app.post('api/df_event_query', (req, res) => {
+    res.send({'do': 'event query'})
+  });
 
 }
