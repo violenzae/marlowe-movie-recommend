f842a71c91d8bca6fd5dabcdc4d1ab4589d6e378
Rachel Schieferstein <violenza@gmail.com>
2020-05-18 11:32:20 -0700

    changing chatbot component to class for establishing local state for the time being

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,7 +10,7 @@ const App = () => {
   return (
   <div>
     <BrowserRouter>
-    <div>
+    <div className="container">
       <Header />
       <Route exact path='/' component={Landing} />
       <Route exact path='/about' component={About} />
--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
-const Chatbot = () => {
-  return (
-    <h2>Chatbot</h2>
-  )
+class Chatbot extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      messages: []
+    }
+  }
+  render(){
+    return (
+      <div style={{height: 400, width: 400, float: 'right'}}>
+        <div id="chatbot" style={{height: '100%', width: '100%', overflow: 'auto'}}>
+          <h2>Chatbot</h2>
+          <input type="text"/>
+        </div>
+      </div>
+    )
+  }
 }
+
 
 export default Chatbot;
