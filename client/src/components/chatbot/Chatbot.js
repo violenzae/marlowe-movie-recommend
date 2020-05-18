ed5d146d611bbc4ef62fdbbda9693922d95f305b
Rachel Schieferstein <violenza@gmail.com>
2020-05-18 16:21:58 -0700

    adding mapping and conditional display ability for card json paylod

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -3,106 +3,158 @@ import axios from 'axios/index';
 import Message from './Message';
 import Cookies from 'universal-cookie';
 import {v4 as uuid} from 'uuid';
+import Card from './Card';
 
 const cookies = new Cookies();
 
 class Chatbot extends React.Component {
   messagesEnd;
-  constructor(props){
+  constructor(props) {
     super(props);
     this.textInput = React.createRef();
     this.state = {
-      messages: []
+      messages: [],
     };
 
-    if (cookies.get('userID') === undefined) {
-      cookies.set('userID', uuid(), {path: '/'});
+    if (cookies.get("userID") === undefined) {
+      cookies.set("userID", uuid(), { path: "/" });
     }
-    console.log(cookies.get('userID'));
+    console.log(cookies.get("userID"));
   }
-  
+
   async df_text_query(text) {
     let says = {
-      speaks: 'me',
+      speaks: "me",
       msg: {
         text: {
-          text: text
-        }
-      }
-    }
+          text: text,
+        },
+      },
+    };
 
-    this.setState({messages: [...this.state.messages, says]});
-    const res = await axios.post('/api/df_text_query', {text: text, userID: cookies.get('userID')});
-    
-    for (let msg of res.data.fulfillmentMessages){
+    this.setState({ messages: [...this.state.messages, says] });
+    const res = await axios.post("/api/df_text_query", {
+      text: text,
+      userID: cookies.get("userID"),
+    });
+
+    for (let msg of res.data.fulfillmentMessages) {
       says = {
-        speaks: 'bot',
-        msg: msg
-      }
-      this.setState({messages: [...this.state.messages, says]});
+        speaks: "bot",
+        msg: msg,
+      };
+      this.setState({ messages: [...this.state.messages, says] });
     }
   }
 
   async df_event_query(event) {
-    const res = await axios.post('/api/df_event_query', {event: event, userID: cookies.get('userID')});
+    const res = await axios.post("/api/df_event_query", {
+      event: event,
+      userID: cookies.get("userID"),
+    });
 
     for (let msg of res.data.fulfillmentMessages) {
       let says = {
-        speaks: 'me',
-        msg: msg
-      }
-      this.setState({messages: [...this.state.messages, says]});
+        speaks: "me",
+        msg: msg,
+      };
+      this.setState({ messages: [...this.state.messages, says] });
     }
-
   }
 
-  componentDidMount(){
-    this.df_event_query('Welcome');
+  componentDidMount() {
+    this.df_event_query("Welcome");
   }
 
-  componentDidUpdate(){
-    this.messagesEnd.scrollIntoView({behavior: 'smooth' });
+  componentDidUpdate() {
+    this.messagesEnd.scrollIntoView({ behavior: "smooth" });
     this.textInput.current.focus();
   }
 
+  renderCards(cards) {
+    return cards.map((card, i) => <Card key={i} payload={card.structValue} />);
+  }
+
+  renderOneMessage(message, i) {
+    if (message.msg && message.msg.text && message.msg.text.text) {
+      return (
+        <Message key={i} speaks={message.speaks} text={message.msg.text.text} />
+      );
+    } else if (message.msg && message.msg.payload.fields.cards) {
+
+      return (
+        <div key={i}>
+          <div className="card-panel grey lighten-5 z-depth-1">
+            <div style={{ overflow: "hidden" }}>
+              <div className="col s2">
+                <a
+                  href="/"
+                  className="btn-floating btn-large waves-effect waves-light red"
+                >
+                  {message.speaks}
+                </a>
+              </div>
+              <div style={{ overflow: "auto", overflowY: "scroll" }}>
+                <div
+                  style={{
+                    height: 300,
+                    width:
+                      message.msg.payload.fields.cards.listValue.values.length *
+                      270,
+                  }}
+                >
+                  {this.renderCards(
+                    message.msg.payload.fields.cards.listValue.values
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+
   renderMessages(stateMessages) {
-    if(stateMessages) {
+    if (stateMessages) {
       return stateMessages.map((message, i) => {
-        if (message.msg && message.msg.text && message.msg.text.text) {
-          return <Message key={i} speaks = {message.speaks} text ={message.msg.text.text} />
-        } else {
-          return (
-          <h2>Cards</h2>
-          )
-        }
-        
-      }) 
-    } else {
+        return this.renderOneMessage(message, i);
+      });
+      } else {
       return null;
     }
   }
-  
-  handleInputKeyPress = (e) => {
-    if (e.key === 'Enter') {
-      this.df_text_query(e.target.value);
-      e.target.value = '';
-      
-    }
-  }
 
-  render(){
+  handleInputKeyPress = (e) => {
+    if (e.key === "Enter") {
+      this.df_text_query(e.target.value);
+      e.target.value = "";
+    }
+  };
+
+  render() {
     return (
-      <div style={{height: 400, width: 400, float: 'right'}}>
-        <div id="chatbot" style={{height: '100%', width: '100%', overflow: 'auto'}}>
+      <div style={{ height: 400, width: 400, float: "right" }}>
+        <div
+          id="chatbot"
+          style={{ height: "100%", width: "100%", overflow: "auto" }}
+        >
           <h2>Chatbot</h2>
           {this.renderMessages(this.state.messages)}
-          <div ref={(el) => {this.messagesEnd = el;}} style={{float: 'left', clear: 'both'}}>
-
-        </div>
-          <input ref={this.textInput} type="text" onKeyPress={this.handleInputKeyPress}/>
+          <div
+            ref={(el) => {
+              this.messagesEnd = el;
+            }}
+            style={{ float: "left", clear: "both" }}
+          ></div>
+          <input
+            ref={this.textInput}
+            type="text"
+            onKeyPress={this.handleInputKeyPress}
+          />
         </div>
       </div>
-    )
+    );
   }
 }
 
