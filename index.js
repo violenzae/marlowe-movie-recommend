7b3a5e419fc6854b35fbdc380de73c0146c95325
Rachel Schieferstein <violenza@gmail.com>
2020-05-20 14:01:07 -0700

    changing routing/port things/scripts for heroku

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ app.use(bodyParser.json());
 
 require('./routes/dialogFlowRoutes')(app);
 
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT); 
 
--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -2,10 +2,6 @@ const chatbot = require('../chatbot/chatbot');
 
 module.exports = app => {
 
-app.get('/', (req, res) => {
-  res.send({'helddslo': 'jodhnny'});
-});
-
 app.post('/api/df_text_query', async (req, res) => {
   let responses = await chatbot.textQuery(req.body.text, req.body.userID, req.body.parameters);
   res.send(responses[0].queryResult)
